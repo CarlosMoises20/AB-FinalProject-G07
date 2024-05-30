63a80ee18f80b90a470d18f0bf1af6d1a9bb573a
dzvicente <dzvicente_98@hotmail.com>
2024-05-30 01:52:28 +0100

    Model Update

--- model.py
+++ model.py
@@ -45,6 +45,54 @@ def define_loss_momentum(loss=0.001, momentum=0.9):
 
 # training
 
+def train_network(num_epochs=100, learning_rate=0.70, momentum=0.9):
+    # Load and preprocess the data
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainloader,testloader,_  = DataLoad.load_data()
+
+    # Initialize the network, criterion, and optimizer
+    net = Net()
+    criterion, optimizer = define_loss_momentum(learning_rate, momentum)
+
+    # Training loop
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+
+            optimizer.zero_grad()  # Zero the parameter gradients
+
+            outputs = net(inputs)  # Forward pass
+            loss = criterion(outputs, labels)  # Compute the loss
+            loss.backward()  # Backward pass
+            optimizer.step()  # Optimize the weights
+
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # Print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('Finished Training')
+
+    # Save the trained model
+
+    return net, trainloader , testloader
+
+
+
+
+# testing
+
+
+net, trainloader, testloader = train_network(num_epochs=100, learning_rate=0.700, momentum=0.9, batch_size=4)
+
+print(trainloader)
+
+
+# validate
 
 
 
